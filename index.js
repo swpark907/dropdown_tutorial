e7cd468f3a5f729c05e34fb3c6f608f1a149a571
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-01-12 17:23:01 +0900

    update displaying value when dropdown item clicked

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const dropDown = document.querySelector('.dropdown');
+const currentType = document.querySelector('.current-type')
+const item = document.querySelectorAll('.item')
+
+let currentValue;
 
 dropDown.addEventListener('click', e =>  {
-  if(dropDown.className.includes('active')){
-    // e.target.
-  }
   dropDown.classList.toggle('active');
 })
+
+item.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    currentValue = e.currentTarget.querySelector('.item_name').innerHTML;
+    currentType.innerHTML = currentValue;
+    // dropDown.classList.remove('active');
+    console.log('removed')
+  })
+}
+  
+)
+
